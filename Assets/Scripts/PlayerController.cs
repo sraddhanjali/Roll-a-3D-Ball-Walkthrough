f60f3c3b235c28863ee4bf766f234335e0f2302f
tilaprimera <tilaprimera@gmail.com>
2017-08-04 15:44:23 -0500

    game init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void Update(){ // this is called before rendering a frame
+		Debug.Log("Called before rendering a frame");
+	}
+
+	void FixedUpdate(){ // called before performing any physics calculations
+		Debug.Log("Called before performing any physics calculations");
+	}
+
+}
