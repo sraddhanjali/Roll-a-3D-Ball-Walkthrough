783387508a1628652e9ec9cb94ddc4ca85e193c0
tilaprimera <tilaprimera@gmail.com>
2017-08-07 16:41:18 -0500

    game done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+public class GameManager : MonoBehaviour {
+
+	private void Update(){
+		if(Input.GetMouseButton(0)){
+			Debug.Log ("found");
+		}
+	}
+}
---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallController : MonoBehaviour {
+
+	public GameObject ground;
+	private int lengthOfGround;
+	private int breadthOfGround;
+
+	// Use this for initialization
+	void Start () {
+		ground = GameObject.Find ("Ground");
+		float sizex = ground.GetComponent<Renderer> ().bounds.size.x;
+		float sizez = ground.GetComponent<Renderer> ().bounds.size.z;
+		Debug.Log (sizex);
+		Debug.Log (sizez);
+	}
+
+}
